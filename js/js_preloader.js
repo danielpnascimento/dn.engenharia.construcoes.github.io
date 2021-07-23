14dfcf734628da27009123effa2138b730b7a53c
Daniel Nascimento <daniel.p.nasciment@gmail.com>
2021-07-22 22:23:23 -0300

    up

---
+++ js/js_preloader.js
@@ -0,0 +1,10 @@
+//Função para inicialização e desativação do loader para que ele não fique rodando
+//em segundo plano
+//var preloader = document.getElementById('loading');
+var preloader = document.getElementById('loader');
+function myFunction() {
+    preloader.style.display = 'none';
+
+}
+
+
